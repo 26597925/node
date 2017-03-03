267f59e53f801c6acc13d70f5e26c0cf63285b87
mazhou <mazhoumz@gmail.com>
2017-03-03 09:41:47 +0800

    change hour and minute

--- main.js
+++ main.js
@@ -4,8 +4,6 @@ var http_requestJS = require('./httprequest/http_get_request.js');
 var reportES = require("./es/reportES.js");
 
 // reportES.debug=true;
-// reportES.debug2=true;
-
 // http_requestJS.offsetday = reportES.offsetday = 2;
 
 var http_get_result=function(result,param,callback){
@@ -14,8 +12,8 @@ var http_get_result=function(result,param,callback){
 
 var rule = new schedule.RecurrenceRule();
 rule.dayOfWeek = [0, new schedule.Range(1, 6)];
-rule.hour = 22;
-rule.minute = 10;
+rule.hour = 0;
+rule.minute = 0;
 
 var j = schedule.scheduleJob(rule, function(){
 　　　http_requestJS.main(http_get_result);
