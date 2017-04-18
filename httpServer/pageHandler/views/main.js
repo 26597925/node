a5c7cc028ede786968a51024999426775837a82e
mazhou <mazhoumz@gmail.com>
2017-04-18 21:12:51 +0800

    json

--- httpServer/pageHandler/views/main.js
+++ httpServer/pageHandler/views/main.js
@@ -1,3 +1,5 @@
+<script language="javascript" type="text/javascript" src="/json2.js"></script>
+
 <script language="javascript" type="text/javascript" src="/public/js/jquery.min.js"></script>
 <script language="javascript" type="text/javascript" src="/public/js/jquery.jqplot.min.js"></script>
 <script type="text/javascript" src="/public/js/jquery-ui-1.8.21.custom.min.js"></script>
