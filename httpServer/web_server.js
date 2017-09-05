078aae5824bf079305c5b3c95ba9bc8942c9ae9a
mazhou <mazhoumz@gmail.com>
2017-09-05 09:35:01 +0800

    change port

--- httpServer/web_server.js
+++ httpServer/web_server.js
@@ -94,8 +94,8 @@ this.broadcast = function (broadcastData) {
 
 exports.runPageServer = function( port )
 {
-	// port = port || 20070;
-	port = port || 80;
+	port = port || 20070;
+	// port = port || 80;
 	console.log(unit_date.getTime(),'Collector Server 127.0.0.1:'+ port );
 	var server = http.createServer(function(req, res){
 	if(req.url == '/upload' && req.method.toLowerCase() == 'post'){
